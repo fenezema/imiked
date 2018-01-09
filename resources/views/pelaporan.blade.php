152d2f4f467ed2e433e735c5a6c82c54c36e060e
Unknown <findryankurnia@gmail.com>
2018-01-09 08:17:13 +0700

    pembaruan tampilan pelaporan

--- resources/views/pelaporan.blade.php
+++ resources/views/pelaporan.blade.php
@@ -23,11 +23,6 @@
             <div class="category-list">
                 
                 <div id="parentVerticalTab">
-                    <div class="agileits-tab_nav">
-                    <ul class="resp-tabs-list hor_1">
-                        <li><i class="icon fa fa-mobile" aria-hidden="true"></i>Lapor</li>
-                    </ul>
-                    </div>
                     <div class="resp-tabs-container hor_1">
                         <!-- tab1 -->
                         <div>
@@ -41,14 +36,14 @@
                         <ol>
                             <li class="text-right">
                                 <label>
-                                    <i class="icon fa fa-camera fa-2x" style="color:#00CED1;">&nbsp;&nbsp;&nbsp;&nbsp;</i>
+                                    <i class="icon fa fa-camera fa-2x" style="color:#FEE901;">&nbsp;&nbsp;&nbsp;&nbsp;</i>
                                     <input id="myFile" class="submit" type="file" name="foto" accept="image/*" capture="camera" hidden required="required">
                                 </label>
                                 <h4 id="camera" class="">Ambil gambar</h4>
                             </li>
 
                             <li>
-                                <h4><i class="icon fa fa-phone-square fa-1x" style="color:#00CED1;"></i>&nbsp;&nbsp;Nomer HP</h4>
+                                <h4><i class="icon fa fa-phone-square fa-1x" style="color:#FEE901;"></i>&nbsp;&nbsp;Nomer HP</h4>
                                 <input type="number" id="tel" name="noTelp" pattern="\d{10}" placeholder="Nomor HP" required="required" />
                                 <p class="validation01">
                                     <span class="invalid">Masukan nomor</span>
@@ -56,7 +51,7 @@
                                 </p>
                             </li>
                             <li>
-                                <h4><i class="fa fa-map-marker fa-1x" aria-hidden="true" style="color:#00CED1"></i>&nbsp;&nbsp;Lokasi</h4>
+                                <h4><i class="fa fa-map-marker fa-1x" aria-hidden="true" style="color:#FEE901"></i>&nbsp;&nbsp;Lokasi</h4>
                                 <h4 id="lokasi2"></h4>
                                 <input type="hidden" id="lat" name="lat" placeholder="Lokasi">
                                 <input type="hidden" id="lon" name="lon" placeholder="Lokasi">
@@ -64,12 +59,12 @@
                                 <div id="ketlokasi">
                                     <input type="text" id="tel" name="ketlok"  placeholder="Detail lokasi" required="required" />
                                 </div>
-                                <button type="button" id="button1" class="btn btn-info" data-toggle="modal" data-target="#myModal1">
-                                    <span class="glyphicon glyphicon-search"></span> Cari lokasi
+                                <button type="button" id="button1" style="color: black; background-color: Transparent; border-color: #FEE901;" class="btn btn-warning">
+                                    <span class="glyphicon glyphicon-search"></span><h4>Cari lokasi</h4>
                                 </button>
                             </li>
                             <li>
-                                <h4><i class="fa fa-pencil-square-o" aria-hidden="true" style="color:#00CED1"></i>&nbsp;&nbsp;Keterangan</h4>
+                                <h4><i class="fa fa-pencil-square-o" aria-hidden="true" style="color:#FEE901"></i>&nbsp;&nbsp;Keterangan</h4>
                                 <textarea style="opacity: 0.7;" name="keterangan" class="form-control" rows="3" placeholder="Tulis Keterangan"></textarea>
                                 <p class="validation01">
                                     <span class="invalid">Keterangan</span>
@@ -77,7 +72,7 @@
                                 </p>
                             </li>
                             <li>
-                                <input style="color: black;" type="submit" class="submit" value="Laporkan" />
+                                <input style="color: black; background-color: #FEE901; font-weight: bold;" type="submit" class="submit" value="Laporkan" />
                             </li>
 
                             <li>
